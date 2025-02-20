305e953c4fb10a72c7c2dd50b31b057e5d1927ea
RishavKaaraTech <rishav.gupta@kaaratech.com>
2025-02-20 16:39:38 +0530

    few small changes
    - addition of flutter_svg

--- lib/utils/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color hintColor = Color(0xFF828282);
-  static const Color textColor = Color(0xff000000);
-  static const Color errorColor = Color(0xffC81240);
-  static const Color borderColor = Color(0xffE0E0E0);
-  static const Color focusedBorderColor = Color(0xff000000);
-}
